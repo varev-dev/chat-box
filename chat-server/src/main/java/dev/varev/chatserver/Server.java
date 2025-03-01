4cdb5b408e4a4b15ed8f1090b07f4d3764faf9e6
Kacper Doga <doga.kacper@gmail.com>
2025-03-01 15:38:20 +0100

    create Server definition, run in Main
    To-fix: Program ends with a SIGINT

---
+++ chat-server/src/main/java/dev/varev/chatserver/Server.java
@@ -0,0 +1,41 @@
+package dev.varev.chatserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Server {
+    private final int port;
+    private static Map<String, Channel> channels;
+
+    public Server(int port) {
+        this.port = port;
+        channels = new ConcurrentHashMap<>();
+    }
+
+    public void run() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server listening on port " + port);
+            while (true) {
+                Socket client = serverSocket.accept();
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+                out.println("Enter channel name: ");
+                String channelName = in.readLine();
+
+                Channel channel = channels.computeIfAbsent(channelName, Channel::new);
+                ClientHandler clientHandler = new ClientHandler(client, channel);
+
+                new Thread(clientHandler).start();
+            }
+        } catch (IOException e) {
+            System.out.println("Server stopping...");
+        }
+    }
+}
