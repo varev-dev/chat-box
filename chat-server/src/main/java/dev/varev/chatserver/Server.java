0d6505912eb28ac4819efad6048cd489de3711e6
Kacper Doga <doga.kacper@gmail.com>
2025-03-04 10:59:18 +0100

    refactor Runner; add State to handle server shutdown

--- chat-server/src/main/java/dev/varev/chatserver/Server.java
+++ chat-server/src/main/java/dev/varev/chatserver/Server.java
@@ -6,41 +6,78 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class Server {
+public class Server implements Runnable {
     private final int port;
     private static Map<String, Channel> channels;
+    private State state;
+
+    public enum State {
+        ON,
+        OFF
+    }
 
     public Server(int port) {
         this.port = port;
         channels = new ConcurrentHashMap<>();
     }
 
+    public void stop() {
+        this.state = State.OFF;
+
+        try {
+            Socket socket = new Socket("127.0.0.1", port);
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Map<String, String> getNameAndChannel(Socket socket) throws IOException {
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        Map<String, String> userData = new HashMap<>();
+
+        out.println("Enter username: ");
+        String userName = in.readLine();
+
+        out.println("Enter channel name: ");
+        String channelName = in.readLine();
+
+        userData.put("username", userName);
+        userData.put("channel", channelName);
+
+        return userData;
+    }
+
+    private void connectUserToChannel(Socket socket, Map<String, String> userData) {
+        if (socket.isConnected()) {
+            Channel channel = channels.computeIfAbsent(userData.get("channel"), Channel::new);
+            String userName = userData.get("username");
+            ClientHandler clientHandler = new ClientHandler(socket, channel, userName);
+
+            System.out.println(userName + " connected to " + "[" + channel.getName() + "]" );
+            var t = new Thread(clientHandler);
+            t.start();
+        }
+    }
+
+    @Override
     public void run() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
+            this.state = State.ON;
             System.out.println("Server listening on port " + port);
 
             while (true) {
                 Socket client = serverSocket.accept();
-                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-
-                out.println("Enter username: ");
-                String userName = in.readLine();
-
-                out.println("Enter channel name: ");
-                String channelName = in.readLine();
-
-                if (client.isConnected()) {
-                    Channel channel = channels.computeIfAbsent(channelName, Channel::new);
-                    ClientHandler clientHandler = new ClientHandler(client, channel, userName);
-
-                    System.out.println(userName + " connected to " + "[" + channel.getName() + "]" );
-                    var t = new Thread(clientHandler);
-                    t.start();
-                }
+                if (this.state == State.OFF)
+                    break;
+                var userData = getNameAndChannel(client);
+                connectUserToChannel(client, userData);
             }
         } catch (IOException e) {
             System.out.println("Server stopping...");
