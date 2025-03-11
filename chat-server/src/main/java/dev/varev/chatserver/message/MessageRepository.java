1e4a9d20378cf7fbeb502f29493dbb3307b8738b
Kacper Doga <doga.kacper@gmail.com>
2025-03-11 23:14:20 +0100

    implement new search methods in MessageRepository for filtering messages

--- chat-server/src/main/java/dev/varev/chatserver/message/MessageRepository.java
+++ chat-server/src/main/java/dev/varev/chatserver/message/MessageRepository.java
@@ -1,8 +1,9 @@
 package dev.varev.chatserver.message;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import dev.varev.chatserver.account.Account;
+import dev.varev.chatserver.channel.Channel;
+
+import java.util.*;
 
 public class MessageRepository {
     private final Map<UUID, Message> messages;
@@ -13,5 +14,31 @@ public class MessageRepository {
 
     public MessageRepository(Map<UUID, Message> messages) {
         this.messages = messages;
+    }
+
+    protected Optional<Message> getMessageByUUID(UUID uuid) {
+        return Optional.ofNullable(messages.get(uuid));
+    }
+
+    protected List<Message> getMessagesBySender(Account sender) {
+        return messages.values().stream()
+                .filter(message -> message.getAuthor() == sender)
+                .sorted(Comparator.comparing(Message::getCreatedAt))
+                .toList();
+    }
+
+    protected List<Message> getMessagesByReceiver(Channel receiver) {
+        return messages.values().stream()
+                .filter(message -> message.getChannel() == receiver)
+                .sorted(Comparator.comparing(Message::getCreatedAt))
+                .toList();
+    }
+
+    protected boolean addMessage(UUID uuid, Message message) {
+        return messages.put(uuid, message) != null;
+    }
+
+    protected boolean removeMessage(UUID uuid) {
+        return messages.remove(uuid) != null;
     }
 }
