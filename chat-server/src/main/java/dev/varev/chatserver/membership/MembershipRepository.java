0edab26723b8e1a58c0a590516a1020e65f267f3
Kacper Doga <doga.kacper@gmail.com>
2025-03-12 15:51:55 +0100

    create default methods for Membership repo and service

--- chat-server/src/main/java/dev/varev/chatserver/membership/MembershipRepository.java
+++ chat-server/src/main/java/dev/varev/chatserver/membership/MembershipRepository.java
@@ -1,6 +1,11 @@
 package dev.varev.chatserver.membership;
 
+import dev.varev.chatserver.account.Account;
+import dev.varev.chatserver.channel.Channel;
+
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class MembershipRepository {
@@ -12,5 +17,24 @@ public class MembershipRepository {
 
     public MembershipRepository(Set<Membership> memberships) {
         this.memberships = memberships;
+    }
+
+    protected Optional<Membership> getMembershipIfActive(Account account, Channel channel) {
+        return getMembership(account, channel).filter(Membership::isActive);
+    }
+
+    protected Optional<Membership> getMembershipIfNotBlocked(Account account, Channel channel) {
+        return getMembership(account, channel).filter(Membership::isBlocked);
+    }
+
+    protected Optional<Membership> getMembership(Account account, Channel channel) {
+        return memberships.stream()
+                .filter(membership -> membership.getAccount().equals(account))
+                .filter(membership -> membership.getChannel().equals(channel))
+                .max(Comparator.comparing(Membership::getJoinedAt));
+    }
+
+    protected boolean addMembership(Membership membership) {
+        return memberships.add(membership);
     }
 }
--- chat-server/src/main/java/dev/varev/chatserver/membership/MembershipService.java
+++ chat-server/src/main/java/dev/varev/chatserver/membership/MembershipService.java
@@ -1,4 +1,53 @@
 package dev.varev.chatserver.membership;
 
+import dev.varev.chatserver.account.Account;
+import dev.varev.chatserver.channel.Channel;
+
+import java.time.Instant;
+import java.util.Optional;
+
 public class MembershipService {
+    private final MembershipRepository repo;
+
+    public MembershipService(MembershipRepository repo) {
+        this.repo = repo;
+    }
+
+    protected Optional<Membership> getActiveMembership(Account account, Channel channel) {
+        return repo.getMembershipIfActive(account, channel);
+    }
+
+    protected boolean addMembership(Account account, Channel channel) {
+        var membership = createMembership(account, channel);
+        return membership.filter(repo::addMembership).isPresent();
+    }
+
+    protected boolean leaveMembership(Account account, Channel channel) {
+        var membership = repo.getMembershipIfActive(account, channel);
+
+        membership.ifPresent(value -> value.setLeftAt(Instant.now()));
+        return membership.isPresent();
+    }
+
+    protected boolean unblockMembership(Account account, Channel channel) {
+        var membership = repo.getMembership(account, channel).filter(Membership::isBlocked);
+
+        membership.ifPresent(value -> value.setBlocked(false));
+        return membership.isPresent();
+    }
+
+    protected boolean blockMembership(Account account, Channel channel) {
+        var membership = repo.getMembershipIfNotBlocked(account, channel);
+
+        membership.ifPresent(value -> {
+            value.setBlocked(true);
+            value.setLeftAt(Instant.now());
+        });
+
+        return membership.isPresent();
+    }
+
+    private Optional<Membership> createMembership(Account account, Channel channel) {
+        return Optional.of(new Membership(account, channel));
+    }
 }
