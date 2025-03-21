5d92ed531b646cb915833dbf99a56bf910289dc0
Kacper Doga <doga.kacper@gmail.com>
2025-03-21 23:59:56 +0100

    add controllers to client

--- chat-server/src/main/java/dev/varev/chatserver/connection/ClientHandler.java
+++ chat-server/src/main/java/dev/varev/chatserver/connection/ClientHandler.java
@@ -1,5 +1,10 @@
 package dev.varev.chatserver.connection;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.varev.chatserver.account.AccountController;
+import dev.varev.chatserver.channel.ChannelController;
+import dev.varev.chatserver.membership.MembershipController;
+import dev.varev.chatserver.message.MessageController;
 import dev.varev.chatserver.message.MessageDTO;
 
 import java.io.BufferedReader;
@@ -12,14 +17,26 @@ public class ClientHandler implements Runnable {
     private final Socket socket;
     private final BufferedReader in;
     private final PrintWriter out;
+    private final ObjectMapper mapper;
 
     private final ConnectionManager connectionManager;
+    private final AccountController accountController;
+    private final ChannelController channelController;
+    private final MembershipController membershipController;
+    private final MessageController messageController;
 
-    public ClientHandler(Socket socket, ConnectionManager connectionManager) throws IOException {
+    public ClientHandler(Socket socket, ObjectMapper mapper, ConnectionManager connectionManager,
+                         AccountController accountController, ChannelController channelController,
+                         MembershipController membershipController, MessageController messageController) throws IOException {
         this.socket = socket;
         this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.out = new PrintWriter(socket.getOutputStream(), true);
+        this.mapper = mapper;
         this.connectionManager = connectionManager;
+        this.accountController = accountController;
+        this.channelController = channelController;
+        this.membershipController = membershipController;
+        this.messageController = messageController;
     }
 
     public void sendMessage(MessageDTO message) {
@@ -28,7 +45,13 @@ public class ClientHandler implements Runnable {
 
     @Override
     public void run() {
+        while (Thread.interrupted()) {
+            continue;
+        }
+    }
 
+    private void parseRequest() throws IOException {
+        
     }
 
     private void closeConnection() {
