a31abe5e3787f5b9cace4ce9e467d4209f0463da
Kacper Doga <doga.kacper@gmail.com>
2025-03-24 11:10:35 +0100

    extract controllers from Client Handler into Dispatcher

--- chat-server/src/main/java/dev/varev/chatserver/connection/ClientHandler.java
+++ chat-server/src/main/java/dev/varev/chatserver/connection/ClientHandler.java
@@ -18,25 +18,16 @@ public class ClientHandler implements Runnable {
     private final BufferedReader in;
     private final PrintWriter out;
     private final ObjectMapper mapper;
-
     private final ConnectionManager connectionManager;
-    private final AccountController accountController;
-    private final ChannelController channelController;
-    private final MembershipController membershipController;
-    private final MessageController messageController;
+    private final RequestDispatcher requestDispatcher;
 
-    public ClientHandler(Socket socket, ObjectMapper mapper, ConnectionManager connectionManager,
-                         AccountController accountController, ChannelController channelController,
-                         MembershipController membershipController, MessageController messageController) throws IOException {
+    public ClientHandler(Socket socket, ObjectMapper mapper, ConnectionManager connectionManager, RequestDispatcher requestDispatcher) throws IOException {
         this.socket = socket;
         this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.out = new PrintWriter(socket.getOutputStream(), true);
         this.mapper = mapper;
         this.connectionManager = connectionManager;
-        this.accountController = accountController;
-        this.channelController = channelController;
-        this.membershipController = membershipController;
-        this.messageController = messageController;
+        this.requestDispatcher = requestDispatcher;
     }
 
     public void sendMessage(MessageDTO message) {
