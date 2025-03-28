2f125d16646debb61c86610f235c1a4bf1fc81c0
Kacper Doga <doga.kacper@gmail.com>
2025-03-28 21:34:08 +0100

    add auth service

---
+++ chat-server/src/main/java/dev/varev/chatserver/authentication/AuthenticationService.java
@@ -0,0 +1,24 @@
+package dev.varev.chatserver.authentication;
+
+import dev.varev.chatserver.account.AccountService;
+import dev.varev.chatserver.connection.ConnectionManager;
+
+import dev.varev.chatshared.AuthenticationDTO;
+
+public class AuthenticationService {
+    private final AccountService accountService;
+    private final ConnectionManager connectionManager;
+
+    public AuthenticationService(AccountService accountService, ConnectionManager connectionManager) {
+        this.accountService = accountService;
+        this.connectionManager = connectionManager;
+    }
+
+    public boolean verify(AuthenticationDTO authenticationDTO) {
+        return false;
+    }
+
+    public boolean register(String username, String password) {
+        return false;
+    }
+}
